097da92feb227aa40d56ed1c262a7f7f03c0dfca
Pandemic Moon <pandemic@moon-network.net>
2016-01-24 12:23:38 -0500

    Testing

--- js/main.js
+++ js/main.js
@@ -347,9 +347,9 @@ function queueNext()
 	console.log(queue);
 	var liToBeQueuedNext = $(this).closest('li');
     var listPosition = liToBeQueuedNext.index();
-    /*var temp = queue[listPosition];
+    /*var temp = queue[listPosition];*/
 	queue[listPosition] = queue[0];
-	queue[0] = temp;*/
+	/*queue[0] = temp;*/
     liToBeQueuedNext.parent().prepend(liToBeQueuedNext);
 	console.log(queue);
 }
